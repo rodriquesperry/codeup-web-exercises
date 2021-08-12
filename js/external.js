0babb15046b66ae454161086fc0ca379ce63f392
Rodriques <rodriques.perry@gmail.com>
2021-08-12 15:40:58 -0500

    update: use alert function and prompt function

--- js/external.js
+++ js/external.js
@@ -1,3 +1,7 @@
 "use strict";
 
 console.log("Hello from external JavaScript.");
+alert('Welcome to my Website!');
+
+let favColor = prompt("What is your favorite color?");
+alert("Great, " + favColor + " is my favorite color too.");
