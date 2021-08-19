3c7e8e035fc90e77b76435ea81f863770b55b7d4
Rodriques <rodriques.perry@gmail.com>
2021-08-19 09:05:03 -0500

    complete: Break and continue exercise 3

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -7,3 +7,12 @@ while (true) {
        break;
    }
 }
+ for (let i = 1; i <= 50; i++) {
+     if (i === parseInt(oddNumber)) {
+         continue;
+     }
+     if (i % 2 === 1) {
+         console.log("Here is an odd number: " + i);
+     }
+ }
+
