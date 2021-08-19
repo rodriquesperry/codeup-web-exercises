a91b69ada60135685cd7362bf8be0b4142bd707c
Rodriques <rodriques.perry@gmail.com>
2021-08-18 20:36:52 -0500

    complete: For Loops Exercises exercise 3

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,3 +5,28 @@ function showMultiplicationTable(num) {
         console.log(num * i);
     }
 }
+
+
+for (let i = 1; i < 10; i++ ) {
+    let rando = Math.floor(Math.random() * 100) + 20;
+    if (rando % 2 === 0) {
+        console.log("Your random number: " + rando + " is even");
+    } else {
+        console.log("Your random number: " + rando + " is odd");
+    }
+}
+
+
+// function pyramid() {
+//     for (let i = 1; i < 10; i++) {
+//         console.log(i.toString().repeat(i));
+//     }
+// }
+// pyramid();
+//
+// for (let i = 100; i >= 0; i--) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
