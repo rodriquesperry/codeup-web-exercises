1d36d15c3ee02e66ed2933826431961bc770e700
Rodriques <rodriques.perry@gmail.com>
2021-08-30 09:49:06 -0500

    complete: math_js exercises

--- js/circle.js
+++ js/circle.js
@@ -32,7 +32,7 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
-    // log info about the circle
+    circle.radius = 5;
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
