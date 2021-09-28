884085c8be4dd8cd403da067065371deafe77b5f
Rodriques <rodriques.perry@gmail.com>
2021-09-28 15:51:14 -0500

    complete: jQuery Exercises through number 3Use jQuery to select an element by the id. Alert the contents of the element.

--- js/jquery_exercise.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-$(document).ready(() => {
-   alert("The DOM has finished loading!");
-});
---
+++ js/jquery_exercises.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// $(document).ready(() => {
+//    alert("The DOM has finished loading!");
+// });
+$('#main-content');
+
