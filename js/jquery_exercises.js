c927f8b85e0b3745f2c570c45612850b029cbe0a
Rodriques <rodriques.perry@gmail.com>
2021-09-28 21:33:53 -0500

    complete: mouse events exercise through number 3.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,10 @@
 "use strict";
 
 $('h1').click(() => {
-    $('h1').css('background-color', "#cd9575")
+    $('h1').css('background-color', "#cd9575");
+});
+
+
+$('p').dblclick(() => {
+    $('p').css('font-size', '18px');
 })
