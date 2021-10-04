a7ccfb2ea66d5ffa32c4340675bf9254fb75df86
Rodriques <rodriques.perry@gmail.com>
2021-10-03 21:54:55 -0500

    complete: Ajax Blog exercise through number 5

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+
+
+
+$.get("data/blog.json").done(function(data) {
+    data.forEach(post => {
+        var html = '<div class="row m-5">'
+                    + '<h3>' + post.title + '</h3>'
+                    + '<p>' + post.content + '</p>'
+                    + '<span class="badge badge-secondary">' + post.categories + '</span>';
+                    + '</div>';
+        $('#posts').append(html)
+    })
+
+})
+
+
+
+
