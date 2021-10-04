9033062a3557d3d350cf269051509289aaeefb99
Rodriques <rodriques.perry@gmail.com>
2021-10-04 16:44:23 -0500

    complete: Mapbox API Exercises through  number 5

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -7,9 +7,14 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-115.1492, 36.1663],
-    zoom: 15
+    zoom: 10
 
 })
+
+var emirils = new mapboxgl.Marker()
+    .setLngLat([-115.169750, 36.102300])
+    .addTo(map);
+
 let latAndLong = geocode("Las Vegas, Nevada", mapboxApiKey2).then(function(results) {
     console.log(results);
 })
