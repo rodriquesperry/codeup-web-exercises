778f8e1e3a57948a00c35d1e63bb41fa8712f20f
Rodriques <rodriques.perry@gmail.com>
2021-10-05 09:28:37 -0500

    update: ajax-blog add join method and seperator to post.categories and change font family

--- js/blog.js
+++ js/blog.js
@@ -6,7 +6,7 @@ $.get("data/blog.json").done(function(data) {
         var html = '<div class="row m-5">'
                     + '<h3>' + post.title + '</h3>'
                     + '<p>' + post.content + '</p>'
-                    + '<span class="badge badge-secondary">' + post.categories + '</span>';
+                    + '<span class="badge badge-secondary">' + post.categories.join(" / ") + '</span>';
                     + '</div>';
         $('#posts').append(html)
     })
