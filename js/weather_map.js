1fc052aa4f2c6ed854834b204658d969ed479da0
Rodriques <rodriques.perry@gmail.com>
2021-10-07 15:29:35 -0500

    complete: Weather Map Project through #5

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,8 +1,48 @@
 
-$.get("http://api.openweathermap.org/data/2.5/weather", {
+$.get("http://api.openweathermap.org/data/2.5/forecast", {
     APPID: openWeatherKey,
-    id:    4726206,
+    lat:    29.425171,
+    lon:    -98.494614,
     units: "imperial"
 }).done(function(data) {
-    console.log(data);
+
+    for (let i = 0; i < data.list.length; i += 8) {
+        let icon = ""
+        let description = ""
+        let wind = data.list[i].wind.speed;
+
+
+        console.log(data.list[i]);
+        data.list[i].weather.forEach(item => {
+            console.log(item.icon);
+            icon = item.icon;
+            description = item.description
+            console.log(item.description);
+            return icon;
+        })
+
+
+
+        let date = new Date(data.list[i].dt * 1000).toLocaleDateString("en-US");
+
+        html = '<div class="card border">'
+                + '<h6 class="card-header text-center">' + date + '</h6>'
+                + '<div class="card-body">'
+                + '<p class="card-title mb-2 text-center">' + data.list[i].main.temp + "&#8457;" + " / " + data.list[i].main.temp_max + "&#8457;" + '</p>'
+                + '<img id="icon" src="http://openweathermap.org/img/w/' + icon + '.png">'
+                + '<hr>'
+                + '<p> Description: <strong>' + description + '</strong></p>'
+                + '<p> Humidity: <strong>' + data.list[i].main.humidity + '</strong></p>'
+                + '<hr>'
+                + '<p> Wind: <strong>' + wind + '</strong></p>'
+                + '<hr>'
+                + '<p> Pressure: <strong>' + data.list[i].main.pressure + '</strong></p>'
+                + '</div>'
+                + '</div>';
+
+        $('.container').append(html);
+    }
+
+
+
 });
