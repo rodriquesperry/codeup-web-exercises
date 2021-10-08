940f5aabb397852733532fb157f6ecccf69c17d9
Rodriques <rodriques.perry@gmail.com>
2021-10-08 12:03:22 -0500

    update: add dynamic current city

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,7 +7,10 @@ weatherData = {
 }
 function getWeather() {
     $.get("http://api.openweathermap.org/data/2.5/forecast", weatherData).done(function(data) {
+        city = '<h3>' + data.city.name + '</h3>';
+        console.log(data.city)
 
+        $('.currCity').append(city);
         for (let i = 0; i < data.list.length; i += 8) {
 
             let icon = ""
@@ -22,16 +25,11 @@ function getWeather() {
 
             let date = new Date(data.list[i].dt * 1000).toLocaleDateString("en-US");
 
-            body = '<div class="container container-weather w-100"></div>'
-                + '<div className="container container-map mt-4">'
-                + '<div id="map"></div>'
-                + '</div>'
-
             html = '<div class="card border">'
                 + '<h6 class="card-header text-center">' + date + '</h6>'
                 + '<div class="card-body">'
                 + '<p class="card-title mb-2 text-center">' + data.list[i].main.temp + "&#8457;" + " / " + data.list[i].main.temp_max + "&#8457;" + '</p>'
-                + '<img id="icon" src="http://openweathermap.org/img/w/' + icon + '.png">'
+                + '<img id="icon" src="http://openweathermap.org/img/wn/' + icon + '@2x.png">'
                 + '<hr>'
                 + '<p> Description: <strong>' + description + '</strong></p>'
                 + '<p> Humidity: <strong>' + data.list[i].main.humidity + '</strong></p>'
@@ -52,7 +50,7 @@ function getWeather() {
     return weatherData;
 }
 
-console.log(getWeather());
+getWeather();
 
 
 mapboxgl.accessToken = mapboxApiKey2;
@@ -61,7 +59,7 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-98.4936, 29.4241],
-    zoom: 15
+    zoom: 5
 })
 
 const marker = new mapboxgl.Marker({
@@ -76,10 +74,10 @@ const onDragEnd = () => {
     weatherData.lon = lngLat.lng;
     map.flyTo({center: [lngLat.lng, lngLat.lat], essential: true});
     console.log(weatherData);
+    $('.currCity').empty();
     $('.container-weather').empty();
-
     getWeather();
-
+    $('.container-weather').scrollTop();
 }
 marker.on('dragend', onDragEnd)
 
