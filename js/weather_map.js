9f3df52506e4b88e3d2f203872e2ea07499e2dec
Rodriques <rodriques.perry@gmail.com>
2021-10-09 10:02:03 -0500

    update: css properties and bootstrap classes

--- js/weather_map.js
+++ js/weather_map.js
@@ -5,10 +5,11 @@ weatherData = {
     lon:    -98.494614,
     units: "imperial"
 }
+
 function getWeather() {
     $.get("http://api.openweathermap.org/data/2.5/forecast", weatherData).done(function(data) {
-        city = '<h5><strong>City:</strong><em>' + " " + data.city.name + '</em></h5>';
-
+        console.log(data);
+        city ='<h5><strong> City:</strong><em>' + " " + data.city.name + '</em></h5>';
 
         $('.currCity').append(city);
         for (let i = 0; i < data.list.length; i += 8) {
@@ -26,32 +27,29 @@ function getWeather() {
             let date = new Date(data.list[i].dt * 1000).toLocaleDateString("en-US");
 
             html = '<div class="card border">'
-                + '<h6 class="card-header text-center">' + date + '</h6>'
+                + '<h6 class="card-header text-center text-white">' + date + '</h6>'
                 + '<div class="card-body">'
-                + '<p class="card-title mb-2 text-center">' + data.list[i].main.temp + "&#8457;" + " / " + data.list[i].main.temp_max + "&#8457;" + '</p>'
-                + '<img id="icon" src="http://openweathermap.org/img/wn/' + icon + '@2x.png">'
+                + '<h5 class="mb-2 text-center"><strong>' + data.list[i].main.temp_max + "&#8457;" + " / " + data.list[i].main.feels_like + "&#8457;" + '</strong></h5>'
+                + '<div class="mt-3" id="iconBkGrnd">'
+                + '<img class="mr-5" id="icon" src="http://openweathermap.org/img/wn/' + icon + '@2x.png">'
+                + '</div>'
                 + '<hr>'
                 + '<p> Description: <strong>' + description + '</strong></p>'
-                + '<p> Humidity: <strong>' + data.list[i].main.humidity + '</strong></p>'
+                + '<p> Humidity: <strong>' + data.list[i].main.humidity + '</strong>%</p>'
                 + '<hr>'
-                + '<p> Wind: <strong>' + wind + '</strong></p>'
+                + '<p> Wind: <strong>' + wind + '</strong> mph</p>'
                 + '<hr>'
                 + '<p> Pressure: <strong>' + data.list[i].main.pressure + '</strong></p>'
                 + '</div>'
                 + '</div>';
 
                 $('.container-weather').append(html);
-
-
-
         }
-
     });
     return weatherData;
 }
 
 getWeather();
-
 
 mapboxgl.accessToken = mapboxApiKey2;
 
@@ -77,18 +75,19 @@ const onDragEnd = () => {
     $('.currCity').empty();
     $('.container-weather').empty();
     getWeather();
-    $('.container-weather').scrollTop();
+    // $('.container-weather').scrollTop();
 }
 
 $('.btn').on('click', function(e) {
     e.preventDefault();
 })
+
 marker.on('dragend', onDragEnd)
 
-    let searchVal = ""
-    let search = $('#search');
-search.change(function() {
+let searchVal = ""
+let search = $('#search');
 
+search.change(function() {
     searchVal = search.val();
     geocode(searchVal, mapboxApiKey2).then(function (result) {
         weatherData.lat = result[1];
