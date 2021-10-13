f5543ba28fe8982e1d2e1a500209b56acbb2c5e2
Rodriques <rodriques.perry@gmail.com>
2021-10-13 09:37:11 -0500

    complete: Todo #8 es6.js

--- js/es6.js
+++ js/es6.js
@@ -81,7 +81,7 @@ users.forEach(user => {
   const { name, email, languages } = user;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
