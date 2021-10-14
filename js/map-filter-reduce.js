32f8018676fa7f65db6d56f0aae30ba9b52bb638
Rodriques <rodriques.perry@gmail.com>
2021-10-14 11:51:50 -0500

    Use .reduce to get the list of user's names in a single string.

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,3 +41,28 @@ console.log(threeLanguages);
 
 let userEmail = users.map(user => user.email);
 console.log(userEmail);
+
+let totalExperience = users.reduce((total, user) => {
+    return total + user.yearsOfExperience;
+}, 0);
+console.log(totalExperience);
+
+let avgExperience = totalExperience/users.length;
+console.log(avgExperience);
+
+let longestEmail = users.reduce((total, user) => {
+    console.log( user.email + " " + user.email.length);
+    if (total.email.length > user.email.length) {
+        return total;
+    } else {
+        return user;
+    }
+}).email;
+console.log(longestEmail);
+
+
+let singleString = users.reduce((name, user) => {
+     return name = [...name, user.name];
+},[]).join(", ");
+console.log(singleString);
+
