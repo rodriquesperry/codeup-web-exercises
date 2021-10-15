67ac4496274e327a3ad6c438dd5275558b035c45
Rodriques <rodriques.perry@gmail.com>
2021-10-15 14:11:02 -0500

    update: Handle An API Promise Exercise through number 3

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,15 @@
 "use strict";
-function username(name) {
-    const url = `https://api.github.com/users/${name}/events`;
-
+function lastCommit(userName) {
+    const url = `https://api.github.com/users/${userName}/events`;
     let gitHubAPI = fetch(url, {headers: {'Authorization': gitHubToken}})
         .then((results)=> results.json())
-        .then(objData => console.log(`Most recent commit is ${objData[0].created_at}`));
+        .then(objData => console.log(`${userName}'s Most recent commit is ${new Date(objData[0].created_at)}`));
     return gitHubAPI
 }
 
-username('rodriquesperry');
-username('jackiedallas');
-username('maryawhite');
+lastCommit('rodriquesperry');
+lastCommit('jackiedallas');
+lastCommit('maryawhite');
 
 
 
